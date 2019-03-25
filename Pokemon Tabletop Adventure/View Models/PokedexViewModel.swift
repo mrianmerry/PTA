24011333ffad51ecde159955b6944f1ba8e6df08
Ian Merryweather <mrianmerry@gmail.com>
2019-03-25 14:02:59 +0000

    Added Sorting Option to Pokedex
    * Defaults to sorting by national pokedex number
    ** Values not dependency-injected, this is probably something that could be done to allow for customisable pokedex views, or for simply configuring the pokedex on creation - saving user sort preferences, etc
    * Also renamed the image assets that were missed in the accented-e purge

--- Pokemon Tabletop Adventure/View Models/PokedexViewModel.swift
+++ Pokemon Tabletop Adventure/View Models/PokedexViewModel.swift
@@ -8,24 +8,44 @@
 
 import UIKit
 
+enum PokemonIdentifier {
+    case nationalPokedex
+    case tabletopAdventure
+
+    var sortTitle: String {
+        return self == .nationalPokedex ? "Tabletop 'dex" : "National 'dex"
+    }
+}
+
 class PokedexViewModel: BaseViewModel {
 
-    private var pokedex: Pokedex
-    let tableManager: TableManager
+    private var pokemon: [Pokemon]
+    var pokemonSorting: PokemonIdentifier = .nationalPokedex
+    lazy var tableManager: TableManager = TableManager(with: "pokedex", rowCount: { [unowned self] _ -> Int in
+        return self.pokemon.count
+    }, sectionCount: { () -> Int in
+        return 1
+    }, cellConfiguration: { [unowned self] cell, index in
+        let pokemon = self.pokemon[index]
+
+        cell.detailTextLabel?.text = pokemon.name
+        let pokemonIdentifier = self.pokemonSorting == .nationalPokedex ? pokemon.dexID : pokemon.ptaID
+        cell.textLabel?.text = pokemonIdentifier.written()
+    })
 
     override init() {
         let pokedex = JSON.pokedex
-        self.pokedex = pokedex
+        let sortedPokemon = pokedex.pokemon.sorted { return $0.dexID < $1.dexID }
+        self.pokemon = sortedPokemon
+    }
 
-        tableManager = TableManager(with: "pokedex", rowCount: { _ -> Int in
-            return pokedex.pokemon.count
-        }, sectionCount: { () -> Int in
-            return 1
-        }, cellConfiguration: { cell, index in
-            let pokemon = pokedex.pokemon[index]
-
-            cell.detailTextLabel?.text = pokemon.name
-            cell.textLabel?.text = pokemon.dexID.written()
-        })
+    func toggleSort() {
+        pokemonSorting = pokemonSorting == .nationalPokedex ? .tabletopAdventure : .nationalPokedex
+        pokemon.sort { first, last -> Bool in
+            switch pokemonSorting {
+            case .nationalPokedex: return first.dexID < last.dexID
+            case .tabletopAdventure: return first.ptaID < last.ptaID
+            }
+        }
     }
 }
